c1374e9233056158100108288d08f61046c6bdb6
Mark Galloway <mgallowa@ualberta.ca>
2015-02-16 11:48:02 -0700

    Refactor models and testing

--- DistributedSocialNetworking/Hindlebook/models.py
+++ DistributedSocialNetworking/Hindlebook/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Author(models.Model):
+    """Model for represting an Author (subclass of User)"""
     user = models.OneToOneField(User)
     github_id = models.CharField(max_length=30, null=True)
     join_date = models.DateField('date joined', auto_now_add=True)
@@ -28,21 +29,30 @@ class Author(models.Model):
 
 
 class Post(models.Model):
+    """Model for representing a Post made by an Author"""
     id = models.AutoField(primary_key=True)
     author = models.ForeignKey(Author)
     pub_date = models.DateTimeField('date published', auto_now_add=True)
     privacy_level = models.IntegerField()
     text = models.TextField()
 
-
-class Image(models.Model):
-    id = models.AutoField(primary_key=True)
-    attached_to = models.ManyToManyField(Post)
+    def __str__(self):
+        return str(self.text)
 
 
 class Comment(models.Model):
+    """Model for representing a Comment on a Post made by an Author"""
     id = models.AutoField(primary_key=True)
     post = models.ForeignKey(Post)
     author = models.ForeignKey(User)
     text = models.CharField(max_length=2048)
     pub_date = models.DateTimeField('date published', auto_now_add=True)
+
+    def __str__(self):
+        return str(self.text)
+
+
+class Image(models.Model):
+    """Model for representing an Image attached to Posts"""
+    id = models.AutoField(primary_key=True)
+    attached_to = models.ManyToManyField(Post, null=True)
--- DistributedSocialNetworking/Hindlebook/tests/test_models.py
+++ DistributedSocialNetworking/Hindlebook/tests/test_models.py
@@ -1,47 +1,101 @@
 from django.test import TestCase
-from Hindlebook.models import Author
+from Hindlebook.models import Author, Post
 from model_mommy import mommy
 
 
 class AuthorTestCases(TestCase):
+    """ Tests related to Author model """
 
+    def setUp(self):
+        self.author1 = mommy.make(Author)
+        self.author2 = mommy.make(Author)
+
+    # Test Author Creation
     def test_author_create(self):
-        a = mommy.make('Hindlebook.Author')
-        self.assertTrue(isinstance(a, Author))
-        self.assertEquals(a.user.username, str(a))
-        self.assertQuerysetEqual(a.follows.all(), [])
-        self.assertQuerysetEqual(a.followed_by.all(), [])
+        self.assertTrue(isinstance(self.author1, Author))
+        self.assertEquals(self.author1.user.username, str(self.author1))
+        self.assertQuerysetEqual(self.author1.follows.all(), [])
+        self.assertQuerysetEqual(self.author1.followed_by.all(), [])
+        self.assertQuerysetEqual(self.author1.getFriends(), [])
+        self.assertQuerysetEqual(self.author1.getFriends(), self.author2.getFriends())
 
-    def test_followers(self):
-        a = mommy.make('Hindlebook.Author')
-        b = mommy.make('Hindlebook.Author')
+    # Test following
+    def test_following(self):
+        # Author1 follows Author2
+        self.author1.follows.add(self.author2)
+        self.assertQuerysetEqual(self.author1.follows.all(), ["<Author: %s>" % self.author2.user.username])
+        self.assertQuerysetEqual(self.author2.followed_by.all(), ["<Author: %s>" % self.author1.user.username])
 
-        a.follows.add(b)
-        self.assertQuerysetEqual(a.follows.all(), ["<Author: %s>" % b.user.username])
-        self.assertQuerysetEqual(b.followed_by.all(), ["<Author: %s>" % a.user.username])
-        self.assertQuerysetEqual(b.follows.all(), [])
-        self.assertQuerysetEqual(a.followed_by.all(), [])
+        # Assert asymmetrical following
+        self.assertQuerysetEqual(self.author2.follows.all(), [])
+        self.assertQuerysetEqual(self.author1.followed_by.all(), [])
 
+    # Test Friends
     def test_friends(self):
-        a = mommy.make('Hindlebook.Author')
-        b = mommy.make('Hindlebook.Author')
+        # Test one way friendship
+        self.author2.follows.add(self.author1)
+        self.assertQuerysetEqual(self.author1.getFriends(), [])
+        self.assertQuerysetEqual(self.author1.getFriends(), self.author2.getFriends())
 
-        self.assertQuerysetEqual(a.getFriends(), [])
-        self.assertQuerysetEqual(a.getFriends(), b.getFriends())
+        # Test reflexive friendship
+        self.author1.follows.add(self.author2)
+        self.assertQuerysetEqual(self.author1.getFriends(), ["<Author: %s>" % self.author2.user.username])
+        self.assertQuerysetEqual(self.author2.getFriends(), ["<Author: %s>" % self.author1.user.username])
 
-        a.follows.add(b)
-        self.assertQuerysetEqual(a.getFriends(), [])
-        self.assertQuerysetEqual(b.getFriends(), [])
-
-        b.follows.add(a)
-        self.assertQuerysetEqual(a.getFriends(), ["<Author: %s>" % b.user.username])
-        self.assertQuerysetEqual(b.getFriends(), ["<Author: %s>" % a.user.username])
-
+    # Test Friend Requests
     def test_friend_requests(self):
-        a = mommy.make('Hindlebook.Author')
-        b = mommy.make('Hindlebook.Author')
+        self.assertQuerysetEqual(self.author1.getFriendRequests(), [])
+        self.author2.follows.add(self.author1)
+        self.assertQuerysetEqual(self.author1.getFriendRequests(), ["<Author: %s>" % self.author2.user.username])
 
-        self.assertQuerysetEqual(a.getFriendRequests(), [])
-        b.follows.add(a)
-        self.assertQuerysetEqual(a.getFriendRequests(), ["<Author: %s>" % b.user.username])
 
+class PostTestCases(TestCase):
+    """Tests Related to Post model"""
+    def setUp(self):
+        self.author1 = mommy.make(Author)
+        self.author2 = mommy.make(Author)
+        self.post1_by_a1 = mommy.make(Post, author=self.author1)
+        self.post2_by_a1 = mommy.make(Post, author=self.author1)
+        self.post1_by_a2 = mommy.make(Post, author=self.author2)
+
+    # Test Post creation
+    def test_post_create(self):
+        # Authors can have multiple posts
+        self.assertEquals(self.post1_by_a1.author.user.username, self.author1.user.username)
+        self.assertEquals(self.post2_by_a1.author.user.username, self.author1.user.username)
+
+    # Test Post privacy
+    def test_post_privacy(self):
+        pass
+
+    # Test fetching own Posts
+    def test_get_own_posts(self):
+        pass
+
+    # Test fetching others Posts
+    def test_get_other_posts(self):
+        pass
+
+
+class CommentTestCases(TestCase):
+    """Tests Related to Comment model"""
+    def setUp(self):
+        self.author1 = mommy.make(Author)
+        self.author2 = mommy.make(Author)
+        self.post_by_a1 = mommy.make(Post, author=self.author1)
+        self.otherpost_by_a1 = mommy.make(Post, author=self.author1)
+        self.post_by_a2 = mommy.make(Post, author=self.author2)
+
+    # Test Comment creation
+    def test_comment_creation(self):
+        pass
+
+
+class ImageTestCases(TestCase):
+    """Tests Related to Image model"""
+    def setUp(self):
+        pass
+
+    # Test Image creation
+    def test_comment_creation(self):
+        pass
