c34dd3657ae16f83ad277c324dd3cf36c48a1358
Tamarabyte <TamaraByte@gmail.com>
2015-03-07 22:47:08 -0700

    User admin, CSS changes

--- DistributedSocialNetworking/DistributedSocialNetworking/urls.py
+++ DistributedSocialNetworking/DistributedSocialNetworking/urls.py
@@ -27,20 +27,3 @@ urlpatterns = patterns('',
     # Admin Site
     url(r'^admin/', include(admin.site.urls)),
 )
-
-"""
-    # Stream URLs
-    url(r'^stream$', login_required(stream), name="stream"),
-
-    # Profile URLs
-    url(r'^profile/(?P<authorID1>[0-9]+)/sendPost', 'Hindlebook.views.statusUpdate', name='statusUpdate'),
-    url(r'^profile/(?P<authorID1>[0-9]+)', 'Hindlebook.views.profileQuery', name='profileQuery'),
-    url(r'^profile', 'Hindlebook.views.myProfile', name='myProfile'),
-
-    # Rest Api
-    url(r'^api/', include('api.urls', namespace='api')),
-    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-
-    # Admin Site
-    url(r'^admin/', include(admin.site.urls)),
-    """
