ffa18aa794813599e4d147c35553f05fcdf5e3e3
Mark Galloway <mgallowa@ualberta.ca>
2015-03-07 23:10:19 -0700

    Fix User Profile

--- DistributedSocialNetworking/DistributedSocialNetworking/urls.py
+++ DistributedSocialNetworking/DistributedSocialNetworking/urls.py
@@ -10,12 +10,12 @@ urlpatterns = patterns('',
     # Pre-login URLs
     url(r'^$', login, {'template_name' : 'login.html', 'authentication_form' : LoginForm}, name='login'),
     url(r'^register$', RegistrationView.as_view(), name='register'),
-    
+
     # Stream URLs
     url(r'^stream$', StreamView.as_view(), name="stream"),
 
     # Profile URLs
-    url(r'^profile/(?P<authorID1>[0-9]+)', ProfileView.as_view()),
+    url(r'^profile/(?P<profileID>[0-9]+)', ProfileView.as_view()),
 
     # Search URLs
     url(r'^search/(?P<query>\w{0,50})/$', 'Hindlebook.views.search', name="search"),
