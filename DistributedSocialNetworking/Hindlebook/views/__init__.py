d5fb8548f303c28bbab9a1202f9f27165a204c9d
Mark Galloway <mgallowa@ualberta.ca>
2015-03-09 13:18:48 -0600

    Fix imports

--- DistributedSocialNetworking/Hindlebook/views/__init__.py
+++ DistributedSocialNetworking/Hindlebook/views/__init__.py
@@ -2,6 +2,4 @@ from Hindlebook.views.stream_view import StreamView, CreatePost, CreateComment
 from Hindlebook.views.auth_views import RegistrationView, LogoutRedirect
 from Hindlebook.views.profile_view import ProfileView, ProfileUpdateView
 from Hindlebook.views.search_view import search
-from Hindlebook.views.post_view import PostView
-from Hindlebook.views.comment_view import CommentView
 from Hindlebook.views.friends_view import FriendsListView
