acbae259947744289cd8373fa69481fe8396adb3
Tamarabyte <Tamarabyte@gmail.com>
2015-03-08 22:34:09 -0600

    Moved Comment Forms into Post Forms to be consitant with model grouping

--- DistributedSocialNetworking/Hindlebook/forms/__init__.py
+++ DistributedSocialNetworking/Hindlebook/forms/__init__.py
@@ -1,3 +1,2 @@
 from Hindlebook.forms.auth_forms import RegistrationForm, LoginForm
-from Hindlebook.forms.post_forms import PostForm
-from Hindlebook.forms.comment_forms import CommentForm
+from Hindlebook.forms.post_forms import PostForm, CommentForm
