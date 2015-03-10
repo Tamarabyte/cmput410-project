46f34eea152add8caa242449ef11731742b20b83
Tamarabyte <Tamarabyte@gmail.com>
2015-03-10 04:23:25 -0600

    CustomSimpleRouter

--- DistributedSocialNetworking/api/routers.py
+++ DistributedSocialNetworking/api/routers.py
@@ -1,10 +1,9 @@
 from rest_framework.routers import Route, DynamicDetailRoute, SimpleRouter
 
-class ReadOnlyRouter(SimpleRouter):
+class CustomSimpleRouter(SimpleRouter):
     """
     A router for read-only APIs, only supports retrieving the list (ex. '/users' or a single user ex. '/user/id')
     """
-    # test
     routes = [
         Route(
             url=r'^{prefix}s$',
@@ -14,7 +13,8 @@ class ReadOnlyRouter(SimpleRouter):
         ),
         Route(
             url=r'^{prefix}/{lookup}$',
-           mapping={'get': 'retrieve'},
+           mapping={'get': 'retrieve',
+                    'put' : 'update'},
            name='{basename}-detail',
            initkwargs={'suffix': 'Detail'}
         ),
