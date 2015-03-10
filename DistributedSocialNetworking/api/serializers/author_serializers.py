d15a697b018478941b8ce1301322e54e14e1b586
Tamarabyte <Tamarabyte@gmail.com>
2015-03-10 03:03:31 -0600

    Author Serializers

--- DistributedSocialNetworking/api/serializers/author_serializers.py
+++ DistributedSocialNetworking/api/serializers/author_serializers.py
@@ -1,13 +1,33 @@
 from django.forms import widgets
 from rest_framework import serializers
-from Hindlebook.models import User
+from Hindlebook.models import User, ForeignUser
 
 
-class UserSerializer(serializers.ModelSerializer):
-    displayname = serializers.CharField('User.username')
-    host = serializers.CharField('User.username')
-    id = serializers.CharField('User.username')
+class AuthorSerializer(serializers.ModelSerializer):
+    """ Used for sending author JSON data to other nodes """
+    displayname = serializers.CharField(source='username')
+    host = serializers.CharField(source='node')
+    id = serializers.CharField(source='uuid')
 
     class Meta:
         model = User;
-        fields = ["displayname, host, id"]
+        fields = ["displayname", "host", "id"]
+
+class ForeignAuthorSerializer(serializers.ModelSerializer):
+    """ Used for sending/retrieving foreign author JSON data to/from other nodes """
+    displayname = serializers.CharField(source='username')
+    host = serializers.CharField(source='node')
+    id = serializers.CharField(source='uuid')
+
+    class Meta:
+        model = ForeignUser;
+        fields = ["displayname", "host", "id"]
+
+class LocalUserSerializer(serializers.ModelSerializer):
+    """ Used locally to allow users to edit their user profile"""
+
+    class Meta:
+        model = User;
+        include = ["uuid", "github_id", "avatar", "about"]
+
+
