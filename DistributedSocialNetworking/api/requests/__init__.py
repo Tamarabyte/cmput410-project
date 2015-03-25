8b1b68e3eeb9f4a09cd8c4578475a1f28e228980
Mark Galloway <mgallowa@ualberta.ca>
2015-03-25 05:51:16 -0600

    implemented PublicPostsRequestFactory

---
+++ DistributedSocialNetworking/api/requests/__init__.py
@@ -0,0 +1,24 @@
+from public_posts_factory import PublicPostsRequestFactory
+
+# Settings for Nodes
+
+HINDLEBOOK = {
+    'host': "hindlebook.tamarabyte.com",
+    'username': "hindlebook.tamarabyte.com",
+    'password': "test",
+    'port': ''
+}
+
+DEV_HINDLEBOOK = {
+    'host': "dev.hindlebook.tamarabyte.com",
+    'username': "dev.hindlebook.tamarabyte.com",
+    'password': "test",
+    'port': ''
+}
+
+TEST = {
+    'host': "localhost",
+    'username': "test",
+    'password': "test",
+    'port': "8000"
+}
