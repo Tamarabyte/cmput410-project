7bdea47a9578d60df7582b68037559b52c6abd9f
Mark Galloway <mgallowa@ualberta.ca>
2015-03-25 10:00:37 -0600

    Nonsaving serializer for Posts

--- DistributedSocialNetworking/api/serializers/post_serializer.py
+++ DistributedSocialNetworking/api/serializers/post_serializer.py
@@ -38,7 +38,7 @@ class PostSerializer(serializers.ModelSerializer):
         uuid = author_data.get('uuid')
         host = author_data.get('node')
         username = author_data.get('username')
-
+        #TODO FIX ME BUG THIS DOESNT SAVE IT WHAT THE FUCK MARK
         author = Author.objects.filter(uuid=uuid).first()
 
         if author is None:
@@ -103,3 +103,79 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ('title', 'source', 'origin', 'description', 'content_type', 'content',
                   'author', 'categories', 'comments', 'pubDate', 'guid', 'visibility')
+
+
+class NonSavingPostSerializer(serializers.ModelSerializer):
+    """
+    A Serializer for the Post Model that doesn't save
+    """
+    comments = CommentSerializer(many=True, read_only=False)
+    author = AuthorSerializer(read_only=False, required=True)
+
+    def get_author(self, author_data):
+        """
+        gets the authors and adds the user/node if necessry
+        """
+        if author_data is None:
+            return None
+
+        # Get Author/Host info
+        uuid = author_data.get('uuid')
+        host = author_data.get('node')
+        username = author_data.get('username')
+
+        author = Author.objects.filter(uuid=uuid).first()
+
+        if author is None:
+            # New foreign author BUG THIS DOESNT SAVE IT WHAT THE FUCK MARK
+            author = Author(uuid=uuid, host=host, username=username)
+
+        return author
+
+    def create_comments(self, post, comment_data):
+        """
+        Creates the comments for `post` stored in `comment_data`
+        """
+        for comment in comment_data:
+            author = comment.pop('author', None)
+            if author is None:
+                raise serializers.ValidationError('The Author field of a Comment is required.')
+            author = self.get_author(author)
+            Comment.objects.create(author=author, post=post, **comment)
+
+    def create(self, validated_data):
+        """
+        Creates and return a new `Post` instance, given the validated data.
+        """
+
+        # Pop nested relationships, we need to handle them separately
+        author_data = validated_data.pop('author')
+        comment_data = validated_data.pop('comments')
+        categories_data = validated_data.pop('categories')
+
+        # Get the Author
+        author = self.get_author(author_data)
+
+        # Create the post
+        post = Post(author=author, **validated_data)
+
+        # Add the categories
+        for category in categories_data:
+            post.categories.add(category)
+
+        # Create the comments
+        # self.create_comments(post, comment_data)
+
+        return post
+
+    def update(self, instance, validated_data):
+        """
+        Not Supported
+        """
+        return None
+
+    class Meta:
+        model = Post
+        fields = ('title', 'source', 'origin', 'description', 'content_type', 'content',
+                  'author', 'categories', 'comments', 'pubDate', 'guid', 'visibility')
+
