eaf9405a558f14ed0244ff3ecfe1f0449fd21c37
Mark Galloway <mgallowa@ualberta.ca>
2015-03-24 22:54:55 -0600

    Refactor Friend Request + Tests

--- DistributedSocialNetworking/api/views/__init__.py
+++ DistributedSocialNetworking/api/views/__init__.py
@@ -1,5 +1,5 @@
-from api.views.views import Friend2Friend, FriendRequest, UnfriendRequest, FollowRequest, UnfollowRequest
+from api.views.views import Friend2Friend, UnfriendRequest, FollowRequest, UnfollowRequest
 from api.views.post_views import PostDetails, PublicPosts, AuthoredPosts, VisiblePosts
 from api.views.author_views import AuthorViewSet
-from api.views.friends_views import FriendQuery
+from api.views.friends_views import FriendQuery, FriendRequest
 
