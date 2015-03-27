bc48786b9acb21548308929974dc727463564207
Tamarabyte <TamaraByte@gmail.com>
2015-03-27 06:35:16 -0600

    disabled UUID validator

--- DistributedSocialNetworking/Hindlebook/models/validators.py
+++ DistributedSocialNetworking/Hindlebook/models/validators.py
@@ -3,10 +3,10 @@ import re
 
 class UuidValidator(RegexValidator):
 
-    regex = re.compile('^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$')
-    message = 'Malformed UUID'
-    code = 'invalid'
-    inverse_match = False
+    #regex = re.compile('^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$')
+    #message = 'Malformed UUID'
+    #code = 'invalid'
+    #inverse_match = False
 
     def __init__(self, **kwargs):
         super(UuidValidator, self).__init__(**kwargs)
